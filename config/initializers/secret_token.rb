f2d02d34e60595ed08a5c66acbbceab68d019a31
eleop <matthieu.aliu@gmail.com>
2014-02-25 00:08:03 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CustomerFollowUp::Application.config.secret_key_base = '946ce89ead664b8889c78a9041e00f9d8c3c617d0161c40c0a9723fe90d43044f88550a3208674050121009f04d3d44e4369b88cf997f643846ef16e05fefc05'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CustomerFollowUp::Application.config.session_store :cookie_store, key: '_customer_follow_up_session'
